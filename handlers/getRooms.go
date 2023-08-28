1e876d79444bcd40a5a779cb24fa9d026e2d9cb7
JoaoRafa19 <joaopedrorafael19@gmail.com>
2023-08-28 16:29:00 -0300

    feat:Metodo que retorna todas as salas disponíveis

--- handlers/getRooms.go
+++ handlers/getRooms.go
@@ -1,9 +1,23 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type RoomReturn struct {
+	rooms []string
+}
 
 func GetRooms(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.Header("Access-Control-Allow-Origin", "*")
+	var data = &RoomReturn{}
+	for key := range rooms {
+		data.rooms = append(data.rooms, key)
+	}
+	// Print the JSON
+
 	c.JSON(200, gin.H{
-		"rooms": rooms,
+		"rooms": data.rooms,
 	})
 }
